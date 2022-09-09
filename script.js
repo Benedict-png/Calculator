0b4132db47df8a1b26b052d5e8a15cf83c1b2959
Benedict-png <111751551+Benedict-png@users.noreply.github.com>
2022-09-09 15:12:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function calculate() {
+    let a = parseInt(document.getElementById("value1").value);
+    let b = parseInt(document.getElementById("value2").value);
+    let oper = document.querySelector("#operator").value;
+
+    let calculate;
+    if(oper === "add") {
+        calculate = a + b;
+    }else if(oper === "sub") {
+        calculate = a - b;
+    }else if(oper === "div") {
+        calculate = a / b;
+    }else if(oper === "mul") {
+        calculate = a * b;
+    }
+
+    document.querySelector(".result").innerHTML = calculate;
+}
